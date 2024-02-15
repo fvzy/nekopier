cb667b16ef80f08f605654b979571e7f5fe1c3b6
Ditzzy <mukhamadnurshodiqarridho_1701617092@mhs.unj.ac.id>
2024-02-15 15:20:34 +0000

    dasdasd

--- socket/socket.js
+++ socket/socket.js
@@ -5,7 +5,6 @@ module.exports=(io)=>{
     io.on('connection', function (socket) {
         socket.on('request', function (data) {
             console.log("Request connection received %s",data.token)
-            // now graphing the website 
             wget(io,data)
         });
       });
